c4984e7c0fddca39e6cddfe94d15b4f114c56474
Raghav Rungta <93864399+loopgoeson@users.noreply.github.com>
2024-03-14 02:06:22 +0530

    initial

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("./data/data");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Api is working");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(4000, console.log(`Server started on PORT ${PORT}`));
